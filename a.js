ebab9b88f21ed59e0d360e958eddbe63703d6835
张磊 <631298983@qq.com>
2022-09-29 17:32:15 +0800

    no message

--- a.js
+++ a.js
@@ -8,16 +8,17 @@ import { fromLonLat } from 'ol/proj'
 import VectorSource from 'ol/source/Vector'
 import VectorLayer from 'ol/layer/Vector'
 import { Circle, Fill, Stroke, Style, Icon } from 'ol/style'
-import axios from 'axios'
-import Featureanimation from 'ol-ext/featureanimation/Drop';
-import {easeOut} from "ol/easing"
+import GeoJSON from 'ol/format/GeoJSON'
+
+import { Select } from 'ol/interaction'
+import { pointerMove } from 'ol/events/condition'
+import { singleClick } from 'ol/events/condition'
+
+import BASEMAP_LAYERS from  "./basemap"
+
 const map = new Map({
   target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM(),
-    }),
-  ],
+  layers:BASEMAP_LAYERS,
   view: new View({
     center: [104.22, 30.23],
     zoom: 10,
@@ -26,52 +27,5 @@ const map = new Map({
 })
 
 
-const style = new Style({
-  image: new Icon({
-    src: '/images/gas/cz_bj_1.png',
-    scale: 0.5,
-  }),
-})
 
 
-function addPoint(data) {
-  const layer = new VectorLayer({
-    source: new VectorSource({
-        features: [],
-
-    })
-})
-
-layer.setStyle(style)
-
-  map.addLayer(layer)
-
-
-  data.forEach((item,i)  => {
-    const feature = new Feature(new Point([item.longitude, item.latitude]))
-    // feature.setStyle(style)
-    pulse(feature,layer)
-
-  })
-}
-
-function pulse(f,layer){
-
-
-
-
-  layer.animateFeature(f, [
-    new Featureanimation({
-    
-    })
-  ]);
-
-
-  layer.getSource().addFeature(f);
-
-}
-
-axios.get('/data/GeoJSON/chemicalEnterprise.json').then((res) => {
-  const data = res.data.data.entList
-  addPoint(data)
-})
